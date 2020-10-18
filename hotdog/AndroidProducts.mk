ba36d7c77f07f1467ff07a937dd9c2c823237e6d
java67 <dakaizhe@yeah.net>
2020-10-18 23:38:21 +0800

    first commit

---
+++ hotdog/AndroidProducts.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosip_hotdog.mk
+COMMON_LUNCH_CHOICES := \
+    aosip_hotdog-userdebug
---
+++ hotdog/device.mk
@@ -0,0 +1,52 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, device/oneplus/sm8150-common/common.mk)
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/oneplus/hotdog/hotdog-vendor.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-derp
+
+# A/B
+AB_OTA_PARTITIONS += \
+    odm \
+    product \
+    recovery \
+    vbmeta_system
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths_pahu.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_pahu.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml
+
+# Camera
+PRODUCT_PACKAGES += \
+    vendor.lineage.camera.motor@1.0 \
+    vendor.lineage.camera.motor@1.0-service.oneplus_msmnile \
+    OnePlusCameraHelper
+
+# Device init scripts
+PRODUCT_PACKAGES += \
+    fstab.qcom
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_SHIPPING_API_LEVEL := 29
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
